d38574b73d86d4891b774b94e8054e2482f05c74
Sonya Vpaa <sonja.vpaa@gmail.com>
2022-03-24 14:28:36 +0200

    updating mobilenav

--- js/main.js
+++ js/main.js
@@ -191,6 +191,7 @@ const navFunc = () => {
 function mobileNavVisible(device) {
   if (device === "mobile" || intro.offsetWidth <= 810) {
     // mobileNav.style.visibility = "visible";
+    console.log("inside MobilenavVisible()");
     beMobile();
 
     // mobileNavIcon.addEventListener("click", (e) => {
